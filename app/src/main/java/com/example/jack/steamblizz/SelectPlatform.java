c138a26d960baf8ec7c7aebec73fd81e40dec777
Jack <Jejames1@myseneca.ca>
2016-10-26 15:58:47 -0400

    all the files that Android Studio fotgot to add

--- app/src/main/java/com/example/jack/steamblizz/SelectPlatform.java
+++ app/src/main/java/com/example/jack/steamblizz/SelectPlatform.java
@@ -1,7 +1,10 @@
 package com.example.jack.steamblizz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SelectPlatform extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class SelectPlatform extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_platform);
+
+        ImageButton Bnet = (ImageButton) findViewById(R.id.BattleNet);
+        ImageButton Steam = (ImageButton) findViewById(R.id.Steam);
+
+        Bnet.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent blizzardIntent = new Intent(v.getContext(), BattleNetDashboard.class);
+                startActivity(blizzardIntent);
+
+            }
+        });
+
+        Steam.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent steamIntent = new Intent(v.getContext(), SteamDasboard.class);
+                startActivity(steamIntent);
+            }
+        });
     }
 }
