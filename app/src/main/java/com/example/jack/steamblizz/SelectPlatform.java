3d984bcb22be6467c993ebb16356060e57751c9a
Jack <Jejames1@myseneca.ca>
2016-10-26 13:38:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jack/steamblizz/SelectPlatform.java
@@ -0,0 +1,13 @@
+package com.example.jack.steamblizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectPlatform extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_platform);
+    }
+}
