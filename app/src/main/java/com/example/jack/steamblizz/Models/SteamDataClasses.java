d95251f6f2339bc784c9b37b2645fa2602d61024
Dang Khue Tran <trandangkhue27@gmail.com>
2016-11-05 17:48:36 -0400

    Edit SteamDataClasses

--- app/src/main/java/com/example/jack/steamblizz/Models/SteamDataClasses.java
+++ app/src/main/java/com/example/jack/steamblizz/Models/SteamDataClasses.java
@@ -1,13 +1,24 @@
 package com.example.jack.steamblizz.Models;
 
+import java.util.Date;
+
 /**
  * Created by TDK on 2016-11-02.
  */
 
 public class SteamDataClasses {
+    class applist {
+        apps apps[];
+    }
+
     class apps {
         Integer appid;
         String name;
+    }
+
+    class appnews {
+        String appid;
+        newsitems news[];
     }
 
     class newsitems {
@@ -18,15 +29,26 @@ public class SteamDataClasses {
         Long date;
     }
 
+    class achievementpercentages {
+        achievements achievements[];
+    }
+
     class achievements {
         String name;
         Double percent;
         String apiname; // for GetPLayerAchievements
-        Integer achieved;
+        Boolean achieved;
         String displayName;
         String description;
         String icon;
         String icongray;
+    }
+
+    class response {
+        players players[];
+        Integer game_count; // for GetOwnedGames
+        Integer total_count; // for GetRecentlyPlayedGames
+        games games[];
     }
 
     class players {
@@ -42,9 +64,20 @@ public class SteamDataClasses {
         String loccountrycode;
     }
 
+    class friendslist {
+        friends friends[];
+    }
+
     class friends {
         String steamid;
         Long friend_since;
+    }
+
+    class playerstats {
+        String steamID;
+        String gameName;
+        achievements achievements[];
+        stats stats[];
     }
 
     class stats {
@@ -57,5 +90,15 @@ public class SteamDataClasses {
         Integer playtime_forever;
         String name; // for GetRecentlyPlayedGames
         Integer playtime_2weeks;
+    }
+
+    class game {
+        String gameName;
+        availableGameStats availableGameStats;
+    }
+
+    class availableGameStats {
+        achievements achievements[];
+        stats stats[];
     }
 }
